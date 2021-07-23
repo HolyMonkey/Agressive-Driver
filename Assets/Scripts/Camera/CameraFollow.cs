651fd51c741cc4fae4f13723fc6bac2c81ded866
Shestakov Nikolay <workjustghostw1ng@gmail.com>
2021-07-23 11:01:17 +0700

    Убрал повороты камеры

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -25,29 +25,5 @@ public class CameraFollow : MonoBehaviour
         transform.position = smoothedPosition;
 
         transform.LookAt(_target.transform);
-
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            StopAllCoroutines();
-            StartCoroutine(Skid(-1));
-        }
-        if (Input.GetKeyUp(KeyCode.A))
-        {
-            StopAllCoroutines();
-            StartCoroutine(Skid(1));
-        }
-
-    }
-
-    IEnumerator Skid(int direction)
-    {
-        rbody.velocity += transform.right * 2 * direction;
-        for (int i = 0; i < 15; i++)
-        {
-            yield return new WaitForFixedUpdate();
-            rbody.velocity += transform.right * 2 * direction;
-        }
-        //yield return new WaitForSeconds(0.5f);
-        //rbody.velocity = transform.right * 20 * direction;
     }
 }
