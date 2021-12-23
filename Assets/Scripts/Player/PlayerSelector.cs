7ec607607fc267e6f29dbc693b9168a00ce678c0
doCent90 <78255120+doCent90@users.noreply.github.com>
2021-12-23 10:02:59 +0300

    Added font & changed/ Corrected PlayerSelector(Buttons)

--- Assets/Scripts/Player/PlayerSelector.cs
+++ Assets/Scripts/Player/PlayerSelector.cs
@@ -1,5 +1,5 @@
+using System;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class PlayerSelector : MonoBehaviour
@@ -7,15 +7,24 @@ public class PlayerSelector : MonoBehaviour
     [SerializeField] private CarData[] _cars;
     [SerializeField] private Image _carImage;
     [SerializeField] private StartGameHider _startGameHider;
+    [Header("Buttons")]
+    [SerializeField] private Button _select;
+    [SerializeField] private Button _nextCar;
+    [SerializeField] private Button _previousCar;
 
     private ButtonsAnimator _buttonsAnimator;
     private int _carIndex = 0;
 
-    public event UnityAction<Player> CarSelected;
+    public event Action<Player> CarSelected;
+    public event Action<Button> ButtonSelected;
 
     private void OnEnable()
     {
         _buttonsAnimator = GetComponentInParent<ButtonsAnimator>();
+
+        _select.onClick.AddListener(SelectCar);
+        _nextCar.onClick.AddListener(NextCar);
+        _previousCar.onClick.AddListener(PreviousCar);
     }
 
     private void Start()
@@ -23,29 +32,41 @@ public class PlayerSelector : MonoBehaviour
         _carImage.sprite = _cars[_carIndex].CarIcon;
     }
 
-    public void NextCar()
+    private void OnDisable()
+    {
+        _select.onClick.RemoveListener(SelectCar);
+        _nextCar.onClick.RemoveListener(NextCar);
+        _previousCar.onClick.RemoveListener(PreviousCar);
+    }
+
+    private void NextCar()
     {
         if(_carIndex < _cars.Length - 1)
         {
             _carIndex++;
             _carImage.sprite = _cars[_carIndex].CarIcon;
-        }   
+        }
+
+        ButtonSelected?.Invoke(_nextCar);
     }
 
-    public void PreviousCar()
+    private void PreviousCar()
     {
         if (_carIndex > 0)
         {
             _carIndex--;
             _carImage.sprite = _cars[_carIndex].CarIcon;
-        }    
+        }
+
+        ButtonSelected?.Invoke(_previousCar);
     }
 
-    public void SelectCar()
+    private void SelectCar()
     {
         CarSelected?.Invoke(_cars[_carIndex].CarPrefab);
         _buttonsAnimator.SetFadeDown();
         _startGameHider.Show();
+
+        ButtonSelected?.Invoke(_select);
     }
 }
-
--- Assets/UI/ScriptsUI/ButtonsAnimator.cs
+++ Assets/UI/ScriptsUI/ButtonsAnimator.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 using DG.Tweening;
-using UnityEngine.UI;
 using System;
+using UnityEngine.UI;
 
 public class ButtonsAnimator : MonoBehaviour
 {
     [SerializeField] private RectTransform[] _rectTransforms;
+    [SerializeField] private PlayerSelector _playerSelector;
     [SerializeField] private CanvasGroup _canvasGroup;
 
     private const float Duration = 1f;
@@ -20,10 +21,17 @@ public class ButtonsAnimator : MonoBehaviour
 
     private void OnEnable()
     {
-        if (_rectTransforms == null)
+        if (_rectTransforms == null || _playerSelector == null || _canvasGroup == null)
             throw new InvalidOperationException();
 
+        _playerSelector.ButtonSelected += SetSize;
+
         Move();
+    }
+
+    private void OnDisable()
+    {
+        _playerSelector.ButtonSelected -= SetSize;
     }
 
     private void Move()
@@ -32,6 +40,11 @@ public class ButtonsAnimator : MonoBehaviour
         {
             rect.DOAnchorPos(Vector3.zero, Duration).SetEase(Ease.InBack);
         }
+    }
+
+    private void SetSize(Button button)
+    {
+
     }
 
     private void DisableCanvas()
