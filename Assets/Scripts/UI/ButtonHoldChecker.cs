6827fbdd953f8c8b52b7d8164fb3673fc697925e
NeverUNICAL <kesarev0@mail.ru>
2022-04-20 20:06:29 +0300

    ButtonsReturn

--- Assets/Scripts/UI/ButtonHoldChecker.cs
+++ Assets/Scripts/UI/ButtonHoldChecker.cs
@@ -1,35 +1,21 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ButtonHoldChecker : MonoBehaviour
+public class ButtonHoldChecker : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
 {
     private bool _isPressed = false;
     
     public bool IsPressed => _isPressed;
-    /*
-    public void OnPointerEnter(PointerEventData eventData)
+    
+    public void OnPointerDown(PointerEventData eventData)
     {
         _isPressed = true;
-    }
-    
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        _isPressed = false;
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
         _isPressed = false;
     }
-    */
     
-    public void SetIsPressed()
-    {
-    _isPressed = true;
-    }
-    
-    public void SetIsPressedFalse()
-    {
-    _isPressed = false;
-    }
+ 
 }
